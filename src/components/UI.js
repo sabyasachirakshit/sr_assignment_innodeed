4701f1852574c35303d9a7611be1f36241da5851
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-11-12 10:42:29 +0530

    UiI base file + base UI css added

---
+++ src/components/UI.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import logo from "./docs/chevron-circled.svg";
+import UIRender from "./UIRender";
+import "./UI.css";
+
+
+
+function UI() {
+  return (
+    <div>UI</div>
+  )
+}
+
+export default UI
