f780776206604c93d0af40cebbf2d39b79382fa1
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-11-12 10:47:57 +0530

    cars added

--- src/components/UI.js
+++ src/components/UI.js
@@ -3,12 +3,39 @@ import logo from "./docs/chevron-circled.svg";
 import UIRender from "./UIRender";
 import "./UI.css";
 
+function UI(props) {
+  const data = props.data;
 
+  const [filteredCars, setFilter] = useState(["suv", "estate", "sedan"]);
+  const [activeCar, setActive] = useState("all");
 
-function UI() {
-  return (
-    <div>UI</div>
-  )
+  const showCars = () => {
+    setFilter(["suv", "estate", "sedan"]);
+    setActive("all");
+  };
+
+  const suvShow = () => {
+    setFilter(["suv"]);
+    setActive("suv");
+  };
+
+  const estateShow = () => {
+    setFilter(["estate"]);
+    setActive("estate");
+  };
+  const sedanShow = () => {
+    setFilter(["sedan"]);
+    setActive("sedan");
+  };
+
+  const previousSlide = () => {
+    let box = document.querySelector(".container");
+    box.scrollLeft = box.scrollLeft - window.innerWidth;
+  };
+  const nextSlide = () => {
+    let box = document.querySelector(".container");
+    box.scrollLeft = box.scrollLeft + window.innerWidth;
+  };
 }
 
-export default UI
+export default UI;
